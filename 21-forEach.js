6660addd303826bf560ea96a9f13dbc221907248
Snehal Pujari <128165298+SnehalSPujari@users.noreply.github.com>
2024-11-29 09:18:32 +0530

    forEach on set and map

--- 21-forEach.js
+++ 21-forEach.js
@@ -11,3 +11,70 @@ array.forEach((currentValue,index, arr)=>{
 
 //from 3 arguments of arrow function 1st is compulsory
 //we can name 3args according to our own
+
+console.log("======= Find Even Numbers ===========");
+
+const arrayNum = [10, 23, 40, 77, 90, 70];
+
+arrayNum.forEach( (element) => {
+
+    if (element%2 == 0) {
+
+        console.log(element);   
+
+    }
+
+} );
+
+console.log("====== Traversing over Set=========");
+
+let mySet = new Set();
+
+mySet.add(10);
+
+mySet.add(20);
+
+mySet.add(30);
+
+mySet.add(40);
+
+mySet.add(50);
+
+// console.log(mySet);
+
+mySet.forEach(element => {
+
+    console.log(element);
+
+    
+
+});
+
+
+console.log("====== Traversing over Map Collection =========");
+
+const myMap = new Map();
+
+myMap.set(11, "Jenny");
+
+myMap.set(22, "Elon");
+
+myMap.set(33, "Stew");
+
+myMap.set(44, "Bill");
+
+myMap.set(22, "Sundar");
+
+myMap.set(55, "Stew");
+
+ 
+
+myMap.forEach((value, key) => {
+
+    console.log(`${key} => ${value}`);
+
+});
+
+
+
+ 
